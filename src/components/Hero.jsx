160ca57830977ca0e3330649b62bb4362798240e
henryprosser <henryprosser@yahoo.co.uk>
2025-03-11 14:26:41 +0000

    Add hero section and styling

---
+++ src/components/Hero.jsx
@@ -0,0 +1,17 @@
+export default function Hero() {
+  return (
+    <section className="hero section">
+      <img
+        src="src/assets/images/mountain-bg.jpg"
+        className="hero__bg"
+        alt="Mountain Background"
+      />
+      <div className="hero__shadow"></div>
+      <div className="hero__container container grid">
+        <div className="hero__data">
+          <h1 className="hero__title">Welcome to My World.</h1>
+        </div>
+      </div>
+    </section>
+  );
+}

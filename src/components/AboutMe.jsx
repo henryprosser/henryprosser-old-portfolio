82a59cf39d84c3d4abef68e5c69f9647c4e9568b
henryprosser <henryprosser@yahoo.co.uk>
2025-03-24 20:09:32 +0000

    Bug fixes and style improvements

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -1,23 +1,24 @@
 export default function AboutMe() {
   return (
-    <section className="about section">
+    <section className="about section" id="about">
+      <h2 className="section__title">About Me</h2>
       <div className="about__container container grid">
+        <div className="headshot__container">
+          <img
+            src="src/assets/images/profile-pic copy.png"
+            alt="Henry Prosser"
+            className="headshot__img"
+          />
+        </div>
         <div className="about__data">
-          <h2 className="section__title">About Me</h2>
-          <div className="headshot__container">
-            <img
-              src="src/assets/images/profile-pic copy.png"
-              alt="Henry Prosser"
-              className="headshot__img"
-            />
-          </div>
           <p className="about__description">
             Hey, I'm Henry 👋 <br></br>
-            <br></br>An aspiring Full-Stack Developer from the UK with a strong
-            foundation in Computer Science. I enjoy building functional and
-            intuitive web applications, whether it's crafting user-friendly
-            interfaces or optimising backend performance. When I'm not coding,
-            you'll find me skiing, playing guitar, or working on side projects.
+            <br></br>An aspiring Full-Stack Developer from the UK with a passion
+            for designing and building functional, creative projects. With a
+            strong foundation in Computer Science and a detail-oriented mindset,
+            I'm always looking for opportunities to learn and develop my skills.
+            When I'm not coding, you'll find me skiing, playing guitar, or
+            working on side projects.
           </p>
           <a href="#" download className="download__button">
             Download CV

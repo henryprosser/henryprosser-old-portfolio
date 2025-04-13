418e9bf30b0abdea18a6fc256f264e534c0771a9
henryprosser <henryprosser@yahoo.co.uk>
2025-04-13 11:59:41 +0100

    Tweak colours for nav scroll header, body background and skill icons

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -27,63 +27,81 @@ export default function Skills() {
         <div className="skill__container">
           <h3 className="skill__title">Front-End</h3>
           <div className="icon__container" id="front-end">
-            <img
-              src="src/assets/images/html.svg"
-              alt="HTML5"
-              className="skill__icon icon1"
-            />
-            <img
-              src="src/assets/images/css.svg"
-              alt="CSS3"
-              className="skill__icon icon2"
-            />
-            <img
-              src="src/assets/images/js.svg"
-              alt="JavaScript"
-              className="skill__icon icon3"
-            />
-            <img
-              src="src/assets/images/react.svg"
-              alt="React"
-              className="skill__icon icon4"
-            />
-            <img
-              src="src/assets/images/vue.svg"
-              alt="Vue"
-              className="skill__icon icon5"
-            />
+            <div class="skill__icon-wrapper icon1">
+              <img
+                src="src/assets/images/html.svg"
+                alt="HTML5"
+                className="skill__icon"
+              />
+            </div>
+            <div class="skill__icon-wrapper icon2">
+              <img
+                src="src/assets/images/css.svg"
+                alt="CSS3"
+                className="skill__icon"
+              />
+            </div>
+            <div class="skill__icon-wrapper icon3">
+              <img
+                src="src/assets/images/js.svg"
+                alt="JavaScript"
+                className="skill__icon"
+              />
+            </div>
+            <div class="skill__icon-wrapper icon4">
+              <img
+                src="src/assets/images/react.svg"
+                alt="React"
+                className="skill__icon"
+              />
+            </div>
+            <div class="skill__icon-wrapper icon5">
+              <img
+                src="src/assets/images/vue.svg"
+                alt="Vue"
+                className="skill__icon"
+              />
+            </div>
           </div>
         </div>
 
         <div className="skill__container">
           <h3 className="skill__title">Back-End</h3>
           <div className="icon__container" id="back-end">
-            <img
-              src="src/assets/images/python.svg"
-              alt="Python"
-              className="skill__icon icon6"
-            />
-            <img
-              src="src/assets/images/java.svg"
-              alt="Java"
-              className="skill__icon icon7"
-            />
+            <div class="skill__icon-wrapper icon6">
+              <img
+                src="src/assets/images/python.svg"
+                alt="Python"
+                className="skill__icon"
+              />
+            </div>
+            <div class="skill__icon-wrapper icon7">
+              <img
+                src="src/assets/images/java.svg"
+                alt="Java"
+                className="skill__icon"
+              />
+            </div>
           </div>
         </div>
 
         <div className="skill__container">
           <h3 className="skill__title">Tools</h3>
           <div className="icon__container" id="tools">
-            <img
-              src="src/assets/images/figma.svg"
-              alt="Python"
-              className="skill__icon icon8"
-            />
-            <img
-              src="src/assets/images/git.svg"
-              alt="Java"
-              className="skill__icon icon9"
-            />
+            <div class="skill__icon-wrapper icon8">
+              <img
+                src="src/assets/images/figma.svg"
+                alt="Python"
+                className="skill__icon"
+              />
+            </div>
+            <div class="skill__icon-wrapper icon9">
+              <img
+                src="src/assets/images/git.svg"
+                alt="Java"
+                className="skill__icon"
+              />
+            </div>
           </div>
         </div>
       </div>

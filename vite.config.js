0099d742a27eb5e6e5a61deaebbe99b8d90f8281
henryprosser <henryprosser@yahoo.co.uk>
2025-04-16 19:46:53 +0100

    Update Vite configuration base path to root

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: "/henryprosser.github.io/",
+  base: "/", // 👈
   plugins: [react()],
 });
